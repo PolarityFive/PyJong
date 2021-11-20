1e41ffdf046d7882e799c2fa0793def812868a68
PolarityFive <39606152+PolarityFive@users.noreply.github.com>
2021-11-20 08:37:46 +0200

    Algorithm now works as intended.
    The algorithm now draws rectangles around all possible matches and waits until the user presses Q to continue.
    
    It's not possible to draw all matches at once because the screen gets way too cluttered and it's impossible to distinguish between boxes. No matter the color.

--- pyjong.py
+++ pyjong.py
@@ -1,19 +1,21 @@
 from PIL import ImageGrab
 import cv2
-import numpy as np
 import glob
 import win32api
 import win32con
 import win32gui
 import pygame
+import os
+import keyboard
+running = True
+
 
 #Window setup
 
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080),pygame.NOFRAME)
-done = False
-fuchsia = (255, 0, 128)  # Transparency color
-dark_red = (0, 0, 255)
+fuchsia = (0, 0, 0)  # Transparency color
+color = (255, 255, 255)
 
 # Create layered window
 hwnd = pygame.display.get_wm_info()["window"]
@@ -24,58 +26,62 @@ l_ex_style |= win32con.WS_EX_TRANSPARENT | win32con.WS_EX_LAYERED
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, l_ex_style)
 
 # Set the window to be transparent and appear always on top
-win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 50, win32con.LWA_ALPHA)  # transparent
+win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 150, win32con.LWA_ALPHA)  # transparent
 win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 1920, 1080, 0) #Always on top
 
+font = pygame.font.SysFont('Arial', 25)
+
+tileNames = [os.path.basename(x) for x in glob.glob('C:\\Users\\Jim\\Desktop\\PyjongIMG\\tiles\\*.png')]
+templates = [cv2.imread(file) for file in glob.glob("C:\\Users\\Jim\\Desktop\\PyjongIMG\\tiles\\*.png")]
 
 
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+threshold = 0.85
+method = cv2.TM_CCOEFF_NORMED
+
+
+while running:
     
+    
+    cImage = ImageGrab.grab()
+    cImage.save('C:\\Users\\Jim\\Desktop\\PyjongIMG\\cImage.png')
     img = cv2.imread('C:\\Users\\Jim\\Desktop\\PyjongIMG\\save1.png',cv2.IMREAD_COLOR)
 
-
-
-    templates = [cv2.imread(file) for file in glob.glob("C:\\Users\\Jim\\Desktop\\PyjongIMG\\tiles\\*.png")]
-    threshold = 0.80
-    method = cv2.TM_CCOEFF_NORMED
-
-
+    i = 0
 
     for template in templates: 
 
         res = cv2.matchTemplate(img, template, method)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-       
-        pygame.draw.rect(screen, dark_red, pygame.Rect(max_loc[0], max_loc[1],50 ,50),3)
-        pygame.display.update()
-
-        resDup = cv2.matchTemplate(img, template, method)
-        min_valDup, max_valDup, min_locDup, max_locDup = cv2.minMaxLoc(resDup)
+        doubleCount = 0
+        singleCount = 2
+        max_val = 1
         
+        isDrawnFlag = False
+        while max_val > threshold:
             
-        
-        
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            if (max_val > threshold):
+                isDrawnFlag = True
+                res[max_loc[1]-30//2:max_loc[1]+30//2+1, max_loc[0]-30//2:max_loc[0]+30//2+1] = 0
+                pygame.draw.rect(screen, color, pygame.Rect(max_loc[0], max_loc[1],50 ,80),3)
+                screen.blit(font.render(tileNames[i], True, (255,255,255)), (max_loc[0]-30, max_loc[1]-30))
+                pygame.display.update()
+                doubleCount+=1
+
         print("Max Val: ", max_val)
-        print("Max ValDup: ", max_valDup)
         print ("Max Loc: ", max_loc)
-        print ("Max LocDup: ", max_locDup)
-
-
-
-
-    #screen.fill(fuchsia)  # Transparent background
+        i += 1 
+        if (isDrawnFlag == True):
+            while(True):
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                if (keyboard.is_pressed('q')):
+                    screen.fill(fuchsia)
+                    pygame.display.update()
+                    isDrawnFlag = False
+                    break
     
-
-
-
-#img = cv2.rectangle(img,(max_loc[0],max_loc[1]), (max_loc[0]+w+1, max_loc[1]+h+1), (255,0,0) )
-#img = cv2.rectangle(img,(max_locDup[0],max_locDup[1]), (max_locDup[0]+w+1, max_locDup[1]+h+1), (255,0,0) )
-#image = ImageGrab.grab()
-#image.save('C:\\Users\\Jim\\Desktop\\PyjongIMG\\image.png')
-
-#cImage = ImageGrab.grab()
-#cImage.save('C:\\Users\\Jim\\Desktop\\PyjongIMG\\cImage.png')
-
